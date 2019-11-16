23c569290f812f7d5a9c71cc8a314696be608e9d
pedro412 <pedro.alvarez412@outlook.com>
2019-11-16 01:40:22 -0600

    login component now redirects to auth pages, navbar initial auth logic

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,11 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.authService.user$) {
+      this.router.navigate(['/inventory']);
+    }
+  }
 }
